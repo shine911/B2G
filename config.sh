14d4885f69f121a0f62b87de8c10ae4c4e6db8b0
shine911 <deathbook1999@gmail.com>
2014-05-10 11:04:38 +0700

    WIP

--- config.sh
+++ config.sh
@@ -1,73 +1,48 @@
 #!/bin/bash
 
 REPO=${REPO:-./repo}
-sync_flags=""
 
 repo_sync() {
-	rm -rf .repo/manifest* &&
-	$REPO init -u $GITREPO -b $BRANCH -m $1.xml $REPO_INIT_FLAGS &&
-	$REPO sync $sync_flags $REPO_SYNC_FLAGS
-	ret=$?
-	if [ "$GITREPO" = "$GIT_TEMP_REPO" ]; then
-		rm -rf $GIT_TEMP_REPO
-	fi
-	if [ $ret -ne 0 ]; then
-		echo Repo sync failed
-		exit -1
-	fi
+rm -rf .repo/manifest* &&
+$REPO init -u $GITREPO -b $BRANCH -m $1.xml &&
+$REPO sync
+ret=$?
+if [ "$GITREPO" = "$GIT_TEMP_REPO" ]; then
+rm -rf $GIT_TEMP_REPO
+fi
+if [ $ret -ne 0 ]; then
+echo Repo sync failed
+exit -1
+fi
 }
 
 case `uname` in
 "Darwin")
-	# Should also work on other BSDs
-	CORE_COUNT=`sysctl -n hw.ncpu`
-	;;
+CORE_COUNT=`system_profiler SPHardwareDataType | grep "Cores:" | sed -e 's/[ a-zA-Z:]*\([0-9]*\)/\1/'`
+;;
 "Linux")
-	CORE_COUNT=`grep processor /proc/cpuinfo | wc -l`
-	;;
+CORE_COUNT=`grep processor /proc/cpuinfo | wc -l`
+;;
 *)
-	echo Unsupported platform: `uname`
-	exit -1
+echo Unsupported platform: `uname`
+exit -1
 esac
 
-GITREPO=${GITREPO:-"git://github.com/mozilla-b2g/b2g-manifest"}
-BRANCH=${BRANCH:-master}
-
-while [ $# -ge 1 ]; do
-	case $1 in
-	-d|-l|-f|-n|-c|-q|-j*)
-		sync_flags="$sync_flags $1"
-		if [ $1 = "-j" ]; then
-			shift
-			sync_flags+=" $1"
-		fi
-		shift
-		;;
-	--help|-h)
-		# The main case statement will give a usage message.
-		break
-		;;
-	-*)
-		echo "$0: unrecognized option $1" >&2
-		exit 1
-		;;
-	*)
-		break
-		;;
-	esac
-done
+#GITREPO=${GITREPO:-"git://github.com/mozilla-b2g/b2g-manifest"}
+GITREPO=${GITREPO:-"git://github.com/shine911/android"}
+BRANCH=${BRANCH:-b2g}
 
 GIT_TEMP_REPO="tmp_manifest_repo"
 if [ -n "$2" ]; then
-	GITREPO=$GIT_TEMP_REPO
-	rm -rf $GITREPO &&
-	git init $GITREPO &&
-	cp $2 $GITREPO/$1.xml &&
-	cd $GITREPO &&
-	git add $1.xml &&
-	git commit -m "manifest" &&
-	git branch -m $BRANCH &&
-	cd ..
+GITREPO=$GIT_TEMP_REPO
+rm -rf $GITREPO &&
+git init $GITREPO &&
+cp $2 $GITREPO/$1.xml &&
+cd $GITREPO &&
+git add $1.xml &&
+git commit -m "manifest" &&
+git branch -m $BRANCH &&
+cd ..
 fi
 
 echo MAKE_FLAGS=-j$((CORE_COUNT + 2)) > .tmp-config
@@ -75,146 +50,91 @@ echo GECKO_OBJDIR=$PWD/objdir-gecko >> .tmp-config
 echo DEVICE_NAME=$1 >> .tmp-config
 
 case "$1" in
+"codina")
+echo DEVICE=codina >> .tmp-config &&
+repo_sync $1
+;;
+
 "galaxy-s2")
-	echo DEVICE=galaxys2 >> .tmp-config &&
-	repo_sync $1
-	;;
+echo DEVICE=galaxys2 >> .tmp-config &&
+repo_sync $1
+;;
 
 "galaxy-nexus")
-	echo DEVICE=maguro >> .tmp-config &&
-	repo_sync $1
-	;;
-
-"nexus-4")
-	echo DEVICE=mako >> .tmp-config &&
-	repo_sync nexus-4
-	;;
-
-"nexus-4-kk")
-	echo DEVICE=mako >> .tmp-config &&
-	repo_sync nexus-4-kk
-	;;
-
-"nexus-5")
-  echo DEVICE=hammerhead >> .tmp-config &&
-  repo_sync nexus-5
-  ;;
+echo DEVICE=maguro >> .tmp-config &&
+repo_sync $1
+;;
 
 "optimus-l5")
-	echo DEVICE=m4 >> .tmp-config &&
-	repo_sync $1
-	;;
+echo DEVICE=m4 >> .tmp-config &&
+repo_sync $1
+;;
 
 "nexus-s")
-	echo DEVICE=crespo >> .tmp-config &&
-	repo_sync $1
-	;;
+echo DEVICE=crespo >> .tmp-config &&
+repo_sync $1
+;;
 
 "nexus-s-4g")
-	echo DEVICE=crespo4g >> .tmp-config &&
-	repo_sync $1
-	;;
+echo DEVICE=crespo4g >> .tmp-config &&
+repo_sync $1
+;;
 
-"otoro"|"unagi"|"keon"|"inari"|"leo"|"hamachi"|"peak"|"helix"|"wasabi"|"flatfish")
-	echo DEVICE=$1 >> .tmp-config &&
-	repo_sync $1
-	;;
-
-"flame")
-	echo PRODUCT_NAME=$1 >> .tmp-config &&
-       repo_sync $1
-	;;
-
-"fugu")
-	echo DEVICE=fugu >> .tmp-config &&
-	echo LUNCH=fugu-eng >> .tmp-config &&
-	echo TARGET_HVGA_ENABLE=true >> .tmp-config &&
-	echo GONK_VERSION=SP7710_13A_W13.39.7 >> .tmp-config &&
-	repo_sync $1
-	;;
-
-"tarako")
-	echo DEVICE=sp6821a_gonk >> .tmp-config &&
-	echo PRODUCT_NAME=sp6821a_gonk >> .tmp-config &&
-	repo_sync $1
-	;;
+"otoro"|"unagi"|"keon"|"inari"|"leo"|"hamachi"|"peak")
+echo DEVICE=$1 >> .tmp-config &&
+repo_sync $1
+;;
 
 "tara")
-	echo DEVICE=sp8810ea >> .tmp-config &&
-	echo LUNCH=sp8810eabase-eng >> .tmp-config &&
-	repo_sync $1
-	;;
-
-"dolphin")
-	echo DEVICE=scx15_sp7715ga >> .tmp-config &&
-	echo PRODUCT_NAME=scx15_sp7715gaplus >> .tmp-config &&
-	repo_sync $1
-	;;
+echo DEVICE=sp8810ea >> .tmp-config &&
+echo LUNCH=sp8810eabase-eng >> .tmp-config &&
+repo_sync $1
+;;
 
 "pandaboard")
-	echo DEVICE=panda >> .tmp-config &&
-	repo_sync $1
-	;;
+echo DEVICE=panda >> .tmp-config &&
+repo_sync $1
+;;
 
-"emulator"|"emulator-jb"|"emulator-kk")
-	echo DEVICE=generic >> .tmp-config &&
-	echo LUNCH=full-eng >> .tmp-config &&
-	repo_sync $1
-	;;
+"emulator")
+echo DEVICE=generic >> .tmp-config &&
+echo LUNCH=full-eng >> .tmp-config &&
+repo_sync $1
+;;
 
-"emulator-x86"|"emulator-x86-jb"|"emulator-x86-kk")
-	echo DEVICE=generic_x86 >> .tmp-config &&
-	echo LUNCH=full_x86-eng >> .tmp-config &&
-	repo_sync $1
-	;;
-
-"flo")
-	echo DEVICE=flo >> .tmp-config &&
-	repo_sync $1
-	;;
+"emulator-x86")
+echo DEVICE=generic_x86 >> .tmp-config &&
+echo LUNCH=full_x86-eng >> .tmp-config &&
+repo_sync emulator
+;;
 
 *)
-	echo "Usage: $0 [-cdflnq] (device name)"
-	echo "Flags are passed through to |./repo sync|."
-	echo
-	echo Valid devices to configure are:
-	echo - galaxy-s2
-	echo - galaxy-nexus
-	echo - nexus-4
-	echo - nexus-4-kk
-	echo - nexus-5
-	echo - nexus-s
-	echo - nexus-s-4g
-	echo - flo "(Nexus 7 2013)"
-	echo - otoro
-	echo - unagi
-	echo - inari
-	echo - keon
-	echo - peak
-	echo - leo
-	echo - hamachi
-	echo - helix
-	echo - wasabi
-	echo - fugu
-	echo - tarako
-	echo - tara
-	echo - dolphin
-	echo - pandaboard
-	echo - flatfish
-	echo - flame
-	echo - emulator
-	echo - emulator-jb
-	echo - emulator-kk
-	echo - emulator-x86
-	echo - emulator-x86-jb
-	echo - emulator-x86-kk
-	exit -1
-	;;
+echo Usage: $0 \(device name\)
+echo
+echo Valid devices to configure are:
+echo - codina
+echo - galaxy-s2
+echo - galaxy-nexus
+echo - nexus-s
+echo - nexus-s-4g
+echo - otoro
+echo - unagi
+echo - inari
+echo - keon
+echo - peak
+echo - leo
+echo - hamachi
+echo - tara
+echo - pandaboard
+echo - emulator
+echo - emulator-x86
+exit -1
+;;
 esac
 
 if [ $? -ne 0 ]; then
-	echo Configuration failed
-	exit -1
+echo Configuration failed
+exit -1
 fi
 
 mv .tmp-config .config
